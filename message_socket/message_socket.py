3b1189c2e234480cbe58b3c1cc1f07c5c3e449f6
Tyrone Lagore <tyronelagore@gmail.com>
2018-11-04 11:48:58 -0800

    first import

---
+++ message_socket/message_socket.py
@@ -0,0 +1,76 @@
+from enum import Enum
+import os
+import pickle
+import platform
+import socket
+import struct
+import sys
+import threading
+import time
+
+class MessageSocket:
+    """Reliable socket to ensure delivery of Messages """
+
+    def __init__(self, socket):
+        """constructor for chat client"""
+        self._socket = socket
+        
+    def send_message(self, message):
+        """ sends a message to be received by another MessageSocket """
+        messageBytes = pickle.dumps(message)
+
+        messageLen = len(messageBytes)
+        header = struct.pack("IIII", messageLen, messageLen, messageLen, messageLen)
+            
+        self._socket.sendall(header)
+        self._socket.sendall(messageBytes)
+
+            
+    def recv_message(self):
+        """ Receives a message and returns it unpickled """
+    
+        header = self.recvall(16) 
+            
+        messageSize = self.get_msg_size(header)
+        messageBytes = self.recvall(messageSize)
+        
+        message = pickle.loads(messageBytes)
+        return message
+
+    def send_raw(self, data):
+        """ """
+        self._socket.sendall(data)
+
+
+    def recv_raw(self, amount):
+        """ """
+        messageBytes = self.recvall(amount)
+        return messageBytes
+
+    def get_msg_size(self, header):
+        """ unpacks header information and returns the length of the message """
+        return struct.unpack("IIII", header)[0]
+    
+    def recvall(self, length):
+        """ receives as many bytes as length from socket """
+        data = bytes([])
+        while len(data) < length:
+            packet = self._socket.recv(length - len(data))
+            if not packet:
+                return None
+            data+= packet
+        return data
+
+
+    def close(self):
+        try:
+            self._socket.close()
+        except:
+            print("!! Error closing socket", file=sys.stderr)
+                                    
+    def __del__(self):
+        """destructor for chat client"""
+        try:
+            self._socket.close()
+        except:
+            print("Error closing socket", file=sys.stderr)
